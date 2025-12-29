ff1dbfa372ff41945e0ef04abcccc77e347cc0d5
dme-rgb <dme@brcommercials.in>
2025-12-29 12:46:35 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
+import * as Sentry from "@sentry/browser";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+Sentry.init({
+  dsn: "https://f2ce44affa5c3714f02266ee31343105@o4510583966072832.ingest.us.sentry.io/4510616880152576",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
